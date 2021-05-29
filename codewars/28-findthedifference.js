e4f70a726c4d3b54a16dfbb0dd0279bb611fc0ab
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-05-29 10:10:50 +0300

    map,sort

--- codewars/28-findthedifference.js
+++ codewars/28-findthedifference.js
@@ -6,7 +6,7 @@ const max = Math.max(...ages);
 for (let i = min; i <= max; i++) {
   difference = max - min;
 }
-  return [min, max, difference];
+  return(min, max, difference);
 }
   console.log(differenceInAges([82, 15, 6, 38, 35]), [6, 82, 76]);
   console.log(differenceInAges([57, 99, 14, 32]), [14, 99, 85]);
